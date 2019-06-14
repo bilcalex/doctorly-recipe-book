df5af7bd5a57c20a8e06754db3124e03fc647d79
bilcalex <bilcalex@gmail.com>
2019-06-14 14:16:47 +0200

    Leftover changes after removing UI
    Changes I forgot to revert after deciding not to go with UI.

--- RecipeApi/Startup.cs
+++ RecipeApi/Startup.cs
@@ -42,8 +42,6 @@ namespace RecipeApi
             app.UseSwagger();
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipe Book API v1"); });
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
